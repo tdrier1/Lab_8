de5c06c97051135d6e2e6af033377a68e4263590
tdrier1 <tdrier1@gmail.com>
2017-04-18 14:56:02 -0400

    Cleaned up code and added methods

--- Lab_8/Program.cs
+++ Lab_8/Program.cs
@@ -10,22 +10,34 @@ namespace Lab_8
     {
         static void Main(string[] args)
         {
+            bool run = true;
+            do
+            {
+                Console.WriteLine("Welcome to the Batting Average Calculator");
+                Console.Write("\nPlease enter the number of at bat: ");
+
+                int bats = Convert.ToInt32(Console.ReadLine());
+
+                double[] batArray = new double[bats];
+
+                Console.WriteLine("\n0=Out, 1=single, 2=double, 3=triple, 4=home run ");
+
+                Calc(bats);
+
+                run = Cont();
+
+            } while (run);
+
+        }
+        public static void Calc(double bats)
+        {
             int result;
             double success = 0;
             double sum = 0;
 
-            Console.WriteLine("Welcome to the Batting Average Calculator");
-            Console.Write("\nPlease enter the number of at bat: ");
-
-            int bats = Convert.ToInt32(Console.ReadLine());
-
-            double[] batArray = new double[bats];
-
-            Console.WriteLine("\n0=Out, 1=single, 2=double, 3=triple, 4=home run ");
-
-            for(int i = 0; i<bats; i++)
-                {
-                Console.Write("Result for at-bat" + i +": ");
+            for (int i = 0; i < bats; i++)
+            {
+                Console.Write("Result for at-bat" + i + ": ");
                 result = Convert.ToInt32(Console.ReadLine());
 
                 if (result > 0)
@@ -40,6 +52,21 @@ namespace Lab_8
 
             Console.WriteLine($"Batting Average:{BA} ");
             Console.WriteLine($"Slugging Percentage:{SP} ");
+        }
+        public static bool Cont()
+        {
+            bool run = true;
+            
+                Console.WriteLine("Do you want to continue?(y/n)");
+                string input = Console.ReadLine();
+
+                if (input == "n")
+                {
+                    Console.WriteLine("\nThank you!");
+                    run = false;
+                }
+            
+            return run;
         }
     }
 }
