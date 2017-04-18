7f38f1c9c12b2026dd37e0e0f2083c1e05fa1959
tdrier1 <tdrier1@gmail.com>
2017-04-18 15:55:09 -0400

    Have array set up

--- Lab_8/Program.cs
+++ Lab_8/Program.cs
@@ -14,45 +14,63 @@ namespace Lab_8
             do
             {
                 Console.WriteLine("Welcome to the Batting Average Calculator");
-                Console.Write("\nPlease enter the number of at bat: ");
 
-                int bats = Convert.ToInt32(Console.ReadLine());
+                Console.Write("\nPlease enter the number of batters: ");
+                int batter = Convert.ToInt32(Console.ReadLine());
 
-                double[] batArray = new double[bats];
+                int[][] batArray = new int[batter][];
 
+                for (int num = 0; num <= batter-1; num++)
+                {
+                    Console.Write("\nPlease enter the number of at bat for batter {0}: ", num);
+                    int bats = Convert.ToInt32(Console.ReadLine());
+                    batArray[num] = new int[bats];
+                }
                 Console.WriteLine("\n0=Out, 1=single, 2=double, 3=triple, 4=home run ");
 
-                Calc(bats);
+                    // read the value of elements from the user 
+                    for (int r = 0; r < batArray.Length; r++)// rows 
+                    {
+                        Console.WriteLine($"Batter.{r + 1}");
+                        for (int c = 0; c < batArray[r].Length; c++)// columns 
+                        {
+                            Console.WriteLine($"Enter result: {c + 1}");
+                            batArray[r][c] = int.Parse(Console.ReadLine());
+                        }
+                    }
+                   
+
 
                 run = Cont();
 
+
             } while (run);
-
         }
-        public static void Calc(double bats)
-        {
-            int result;
-            double success = 0;
-            double sum = 0;
+     
+        //public static void Calc(double bats)
+        //{
+        //    int result;
+        //    double success = 0;
+        //    double sum = 0;
 
-            for (int i = 0; i < bats; i++)
-            {
-                Console.Write("Result for at-bat" + i + ": ");
-                result = Convert.ToInt32(Console.ReadLine());
+        //    for (int i = 0; i < bats; i++)
+        //    {
+        //        Console.Write("Result for at-bat" + i + ": ");
+        //        result = Convert.ToInt32(Console.ReadLine());
 
-                if (result > 0)
-                {
-                    success++;
-                    sum = sum + result;
-                }
-            }
+        //        if (result > 0)
+        //        {
+        //            success++;
+        //            sum = sum + result;
+        //        }
+        //    }
 
-            double BA = success / bats;
-            double SP = sum / bats;
+        //    double BA = success / bats;
+        //    double SP = sum / bats;
 
-            Console.WriteLine($"Batting Average:{BA} ");
-            Console.WriteLine($"Slugging Percentage:{SP} ");
-        }
+        //    Console.WriteLine($"Batting Average:{BA} ");
+        //    Console.WriteLine($"Slugging Percentage:{SP} ");
+        //}
         public static bool Cont()
         {
             bool run = true;
