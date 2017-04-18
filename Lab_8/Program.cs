ed9c1a881363aec3e200dacf07f4056fc3a151a2
tdrier1 <tdrier1@gmail.com>
2017-04-18 17:00:11 -0400

    Uploading for Jonas to work on tonight

--- Lab_8/Program.cs
+++ Lab_8/Program.cs
@@ -13,6 +13,14 @@ namespace Lab_8
             bool run = true;
             do
             {
+                int result;
+                double success = 0;
+                double sum = 0;
+                int c;
+                int numberOfSuccess;
+                int r;
+                int bats = 0;
+
                 Console.WriteLine("Welcome to the Batting Average Calculator");
 
                 Console.Write("\nPlease enter the number of batters: ");
@@ -23,53 +31,65 @@ namespace Lab_8
                 for (int num = 0; num <= batter-1; num++)
                 {
                     Console.Write("\nPlease enter the number of at bat for batter {0}: ", num);
-                    int bats = Convert.ToInt32(Console.ReadLine());
+                    bats = Convert.ToInt32(Console.ReadLine());
                     batArray[num] = new int[bats];
                 }
+
                 Console.WriteLine("\n0=Out, 1=single, 2=double, 3=triple, 4=home run ");
 
+                for(int t = 0; t < bats; t++)
+                { 
+                for (int b = 0; b < batter; b++)
+                {
                     // read the value of elements from the user 
-                    for (int r = 0; r < batArray.Length; r++)// rows 
+                    for (r = 0; r < batArray.Length; r++)// rows 
                     {
-                        Console.WriteLine($"Batter.{r + 1}");
-                        for (int c = 0; c < batArray[r].Length; c++)// columns 
+                        Console.Write($"Batter.{r + 1}");
+                        for (c = 0; c < batArray[r].Length; c++)// columns 
                         {
-                            Console.WriteLine($"Enter result: {c + 1}");
+                            Console.Write($"Enter result: {c + 1}");
                             batArray[r][c] = int.Parse(Console.ReadLine());
+
+                            if (batArray[r][c] > 0)
+                            {
+                                success++;
+                                sum = sum + batArray[r][c];
+                            }
                         }
                     }
-                   
+
+                    
+                }
+                    Console.WriteLine("BAT ARRAY{0}", batArray[0][t]);
+                }
+
+
+                //double BA = success / batArray[r][c];
+                //double SP = sum / batArray[r][c];
+                //Console.WriteLine($"Batting Average:{BA} ");
+                //Console.WriteLine($"Slugging Percentage:{SP} ");
+
+
+
+                //for (int i = 0; i < batArray[]; i++)
+                //{
+
+                //    result = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+
 
 
                 run = Cont();
 
-
             } while (run);
         }
-     
+
         //public static void Calc(double bats)
         //{
-        //    int result;
-        //    double success = 0;
-        //    double sum = 0;
-
-        //    for (int i = 0; i < bats; i++)
-        //    {
-        //        Console.Write("Result for at-bat" + i + ": ");
-        //        result = Convert.ToInt32(Console.ReadLine());
-
-        //        if (result > 0)
-        //        {
-        //            success++;
-        //            sum = sum + result;
-        //        }
-        //    }
-
-        //    double BA = success / bats;
-        //    double SP = sum / bats;
-
-        //    Console.WriteLine($"Batting Average:{BA} ");
-        //    Console.WriteLine($"Slugging Percentage:{SP} ");
+            
         //}
         public static bool Cont()
         {
